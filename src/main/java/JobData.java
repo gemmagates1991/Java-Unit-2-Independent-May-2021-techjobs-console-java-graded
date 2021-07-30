d8c0cc79a3dabb8f5f09b6430fc35ef2f957b652
gemmagates1991 <gemmagates1991@gmail.com>
2021-07-30 11:34:57 -0500

    updates made

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -77,9 +77,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -95,11 +95,22 @@ public class JobData {
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
+        value = value.toLowerCase();
+
         // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+            for(String key : job.keySet()) {
+                if(job.get(key).toLowerCase().contains(value)) {
+                    jobs.add(job);
+                    break;
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()){
+            System.out.print("No Results");
+        } else {
+            for (HashMap<String, String> i:someJobs){
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> j : i.entrySet()){
+                    System.out.println(j.getKey() + ": " + j.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
+
+        //System.out.println("printJobs is not implemented yet");
     }
 }
